7421f3477aa4e110b267042ed29f9c316e3d8b7e
Geppix140269 <139779768+Geppix140269@users.noreply.github.com>
2025-06-30 09:49:00 +0200

    Create utils.ts

---
+++ src/lib/utils.ts
@@ -0,0 +1,19 @@
+import { type ClassValue, clsx } from "clsx"
+import { twMerge } from "tailwind-merge"
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs))
+}
+
+export function formatPrice(price: number): string {
+  return new Intl.NumberFormat('it-IT', {
+    style: 'currency',
+    currency: 'EUR',
+    minimumFractionDigits: 0,
+    maximumFractionDigits: 0,
+  }).format(price)
+}
+
+export function formatArea(sqm: number): string {
+  return `${sqm} m²`
+}
